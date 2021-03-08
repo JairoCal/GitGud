54857c35eb29065c0ece1c2053f7625d35c71be1
nathaniel <nathanieljortega@gmail.com>
2021-03-08 12:54:28 -0600

    created user-login.pug, created utils.js, and updated user.js

--- routes/users.js
+++ routes/users.js
@@ -9,4 +9,8 @@ router.get('/login', csrfProtection, asyncHandler(async (req, res, next) => {
   res.render('user-login', {title: 'User Login', csrfToken: req.csrfToken()});
 }));
 
+router.post('/login', csrfProtection, asyncHandler(async (req, res, next) => {
+  
+}));
+
 module.exports = router;
